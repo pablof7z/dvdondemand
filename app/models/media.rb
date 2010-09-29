56b45eddf74d65d4a2ff768242290e609fc8c45e
carakan <carakan@gmail.com>
2010-09-29 15:59:26 -0400

    removed model media and files

--- app/models/media.rb
+++
@@ -1,4 +0,0 @@
-class Media < ActiveRecord::Base
-  has_one :cover_art
-  has_and_belongs_to_many :catalog_media
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :medias
-
   map.resources :order_items
 
   map.resources :shipments
