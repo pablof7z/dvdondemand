bea294900e9d9119e1cebb0e7d1bd174c054e766
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-09-29 19:08:31 -0300

    updated Payment resource; see new models.png

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ActiveRecord::Base
   belongs_to :publisher
   belongs_to :customer
-  belongs_to :shipping_option
+  belongs_to :shipping_option # USPS, FedEx, DHL, etc.
   has_many :order_items
+  has_many :payments
 end
--- db/migrate/20100929154954_create_payments.rb
+++ db/migrate/20100929154954_create_payments.rb
@@ -1,7 +1,8 @@
 class CreatePayments < ActiveRecord::Migration
   def self.up
     create_table :payments do |t|
-
+      t.float :amount
+      t.references :order
       t.timestamps
     end
   end
