5bc2da78be842cf6f7dd3a071a4eed2ceb1170f8
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-10-06 11:07:59 -0300

    added Cover art upload support to Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,9 @@ class Product < ActiveRecord::Base
   has_many :product_flags
 
   accepts_nested_attributes_for :wholesale_prices, :reject_if => lambda { |w| w[:minimum_quantity].blank? || w[:discount_percentage].blank? }
+
+  # cover art image uploads support thanks to Paperclip gem
+  has_attached_file :cover, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates_attachment_content_type :cover, :content_type => ['image/jpeg', 'image/png', 'image/pjpeg', 'image/x-png'] # latter for IE support
 end
+
