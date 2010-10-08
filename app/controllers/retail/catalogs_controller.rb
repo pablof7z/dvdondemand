eed4a78dacaa79b33446a879bcb2085ec28869b9
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-10-08 18:45:24 -0300

    first retail private-Catalog protection

--- app/controllers/retail/catalogs_controller.rb
+++ app/controllers/retail/catalogs_controller.rb
@@ -15,10 +15,19 @@ class Retail::CatalogsController < ApplicationController
 
   def show
     show! do |format|
+      render(:text => 'Forbidden', :status => 403) and return if @catalog.private
+
       format.xml do
         render :xml => @catalog.to_xml(:except => [:private, :password, :publisher_id, :updated_at], :include => { :publisher => {:except => [:id, :email, :approved, :created_at, :updated_at]}, :products => {:except => [:catalog_id, :publisher_id, :cover_file_size, :cover_updated_at]} })
       end
     end
+  end
+
+  protected
+
+  def collection
+    # by default, only show non-private (public) Catalogs
+    @catalogs = Catalog.public
   end
 end
 
