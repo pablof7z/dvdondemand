87cc8fb81f46ed1b8ee491c12e9faf99a6d96f22
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-10-08 09:08:06 -0300

    added XML responses to retail Catalogs

--- app/controllers/retail/catalogs_controller.rb
+++ app/controllers/retail/catalogs_controller.rb
@@ -1,7 +1,24 @@
 class Retail::CatalogsController < ApplicationController
   inherit_resources
   actions :index, :show
+  respond_to :xml
 
   layout 'retail'
+
+  def index
+    index! do |format|
+      format.xml do
+        render :xml => @catalogs.to_xml(:except => [:private, :password, :publisher_id, :updated_at], :include => { :publisher => {:except => [:id, :email, :approved, :created_at, :updated_at]} })
+      end
+    end
+  end
+
+  def show
+    show! do |format|
+      format.xml do
+        render :xml => @catalog.to_xml(:except => [:private, :password, :publisher_id, :updated_at], :include => { :publisher => {:except => [:id, :email, :approved, :created_at, :updated_at]}, :products => {:except => [:catalog_id, :publisher_id, :cover_file_size, :cover_updated_at]} })
+      end
+    end
+  end
 end
 
