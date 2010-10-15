1bd230aebbe01cd85035f6dfba33a449164529e2
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-10-15 14:00:38 -0300

    auth-selective Products CRUD according to current Devise-Publisher

--- app/controllers/publish/products_controller.rb
+++ app/controllers/publish/products_controller.rb
@@ -2,5 +2,11 @@ class Publish::ProductsController < Publish::PublishController
   belongs_to :publisher do
     belongs_to :catalog, :optional => true
   end
+
+  protected
+
+  def collection
+    @products = current_publisher.products
+  end
 end
 
