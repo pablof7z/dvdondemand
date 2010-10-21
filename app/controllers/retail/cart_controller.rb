8b508974b00523310c83858b87ada1e17eda658e
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-10-21 10:44:52 -0300

    added Cart helper for updated views & Retail layout

--- app/controllers/retail/cart_controller.rb
+++ app/controllers/retail/cart_controller.rb
@@ -2,11 +2,7 @@ class Retail::CartController < ApplicationController
   before_filter :authenticate_customer!, :find_cart_items
   layout 'retail'
 
-  def index
-    @cart_total = customer_session[:cart_items].inject(0) do |total, item|
-      total + Product.find(item['product_id']).price*item['qty'].to_f
-    end
-  end
+  def index; end
 
   def add_item
     customer_session[:cart_items] << params[:item]
---
+++ app/helpers/retail/cart_helper.rb
@@ -0,0 +1,22 @@
+module Retail::CartHelper
+  def cart_total
+    total = 0
+    unless customer_session[:cart_items].blank?
+      total = customer_session[:cart_items].inject(0) do |total, item|
+        total + item['price'].to_f*item['qty'].to_f
+      end
+    end
+    total
+  end
+
+  def cart_item_count
+    count = 0
+    unless customer_session[:cart_items].blank?
+      count = customer_session[:cart_items].inject(0) do |count, item|
+        count + item['qty'].to_i
+      end
+    end
+    count
+  end
+end
+
