6bd5ace460c13d6c7d78436141c9b750e96a52c3
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-10-21 23:22:42 -0300

    added delete item from Cart

--- app/controllers/retail/cart_controller.rb
+++ app/controllers/retail/cart_controller.rb
@@ -9,6 +9,13 @@ class Retail::CartController < ApplicationController
     redirect_to :action => :index
   end
 
+  def del_item
+    unless params[:product_id].blank?
+      customer_session[:cart_items].delete_if { |item| item['product_id'] == params[:product_id]  }
+    end
+    redirect_to :action => :index
+  end
+
   private
 
   def find_cart_items
--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,7 @@ ActionController::Routing::Routes.draw do |map|
 
     retail.cart     'cart',     :controller => 'cart', :action => 'index'
     retail.cart_add 'cart_add', :controller => 'cart', :action => 'add_item'
-    retail.cart_del 'cart_del', :controller => 'cart', :action => 'del_item'
+    retail.cart_del 'cart_del/:product_id', :controller => 'cart', :action => 'del_item'
   end
 
   map.root :controller => 'retail/catalogs'
