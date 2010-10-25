07ed037a0997ec17fd14c05d505d6b532e9d654b
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-10-25 13:15:07 -0300

    first working Order creation from Cart contents

--- app/controllers/retail/orders_controller.rb
+++ app/controllers/retail/orders_controller.rb
@@ -1,5 +1,9 @@
 class Retail::OrdersController < Retail::RetailController
   before_filter :authenticate_customer!
   belongs_to :customer
+
+  def create
+    create! { customer_session[:cart_items] = [] }
+  end
 end
 
