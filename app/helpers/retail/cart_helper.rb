e9df51c000f65dad71ec843bd57e9a39a64fa7a6
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-10-25 00:41:10 -0300

    protected fron adding duplicate Products to Cart

--- app/helpers/retail/cart_helper.rb
+++ app/helpers/retail/cart_helper.rb
@@ -2,8 +2,8 @@ module Retail::CartHelper
   def cart_total
     total = 0
     unless customer_session[:cart_items].blank?
-      total = customer_session[:cart_items].inject(0) do |total, item|
-        total + item['price'].to_f*item['qty'].to_f
+      total = customer_session[:cart_items].inject(0) do |subtotal, item|
+        subtotal + item['price'].to_f*item['qty'].to_f
       end
     end
     total
@@ -12,11 +12,18 @@ module Retail::CartHelper
   def cart_item_count
     count = 0
     unless customer_session[:cart_items].blank?
-      count = customer_session[:cart_items].inject(0) do |count, item|
-        count + item['qty'].to_i
+      count = customer_session[:cart_items].inject(0) do |subtotal, item|
+        subtotal + item['qty'].to_i
       end
     end
     count
+  end
+
+  def cart_items_include?(product_id)
+    customer_session[:cart_items].each do |item|
+      return true if item['product_id'] == product_id.to_s
+    end
+    false
   end
 end
 
