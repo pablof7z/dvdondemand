137df12e4922f50e2a39946997bf99e6524edac8
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-10-26 19:33:34 -0300

    Product model associated with Genre, inc. fixtures

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,6 @@
 class Genre < ActiveRecord::Base
+  has_many :products
+
   validates_presence_of :title
   validates_uniqueness_of :title, :scope => :media
 end
--- db/migrate/20100928232918_create_products.rb
+++ db/migrate/20100928232918_create_products.rb
@@ -1,13 +1,13 @@
 class CreateProducts < ActiveRecord::Migration
   def self.up
     create_table :products do |t|
-      t.string :product_type
+      t.references :publisher
+      t.references :genre
       t.string :title
       t.string :studio
       t.text :description
       t.float :price
       t.integer :running_time
-      t.references :publisher
       t.timestamps
     end
   end
@@ -16,3 +16,4 @@ class CreateProducts < ActiveRecord::Migration
     drop_table :products
   end
 end
+
