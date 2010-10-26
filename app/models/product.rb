4af646e07c18d49213ab19ebfc0e4219b880ef6b
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-10-26 20:28:27 -0300

    fixed Genre associations, added to publisher's Product form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,14 @@
 class Product < ActiveRecord::Base
   belongs_to :publisher
-  has_one :genre
+  belongs_to :genre
+
   has_many :order_items
+  has_many :product_flags  # customers may flag a product
+
   has_and_belongs_to_many :catalogs
   has_and_belongs_to_many :items
-  has_many :product_flags
+
+  validates_presence_of :genre
 
   has_attached_file :cover, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :cover, :content_type => ['image/jpeg', 'image/png', 'image/pjpeg', 'image/x-png'] # latter for IE support
