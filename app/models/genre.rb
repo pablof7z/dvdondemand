6a13ebabd5fd425d74df0f29d4c3e2b3604f153b
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-10-26 21:28:54 -0300

    added products' Genres CRUD to Admin module

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,8 @@
 class Genre < ActiveRecord::Base
   has_many :products
 
+  default_scope :order => 'media, title'
+
   validates_presence_of :title
   validates_uniqueness_of :title, :scope => :media
 end
