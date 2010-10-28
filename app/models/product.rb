2b217ab4ea74171c947550c497ce1a288fef4e0f
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-10-28 13:48:12 -0300

    added acts-as-taggable-on Gem dependency for Keywords support on Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ActiveRecord::Base
   belongs_to :genre
 
   has_many :order_items
-  has_many :product_flags  # customers may flag a product
+  has_many :product_flags  # customers may flag a product for provided reasons
 
   has_and_belongs_to_many :catalogs
   has_and_belongs_to_many :items
@@ -13,6 +13,9 @@ class Product < ActiveRecord::Base
 
   has_attached_file :cover, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates_attachment_content_type :cover, :content_type => ['image/jpeg', 'image/png', 'image/pjpeg', 'image/x-png'] # latter for IE support
+
+  acts_as_taggable
+  acts_as_taggable_on :keywords
 
   def cd?
     # do not check thru MediaType association to make comparison snappier
--- config/environment.rb
+++ config/environment.rb
@@ -19,10 +19,11 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "haml"
+  config.gem "acts-as-taggable-on"
   config.gem "devise"
-  config.gem "paperclip"
+  config.gem "haml"
   config.gem "inherited_resources"
+  config.gem "paperclip"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
