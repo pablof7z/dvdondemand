1928330f7c0f4de5382214f86354e97f8f1aec3f
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-10-29 09:49:42 -0300

    bare functional test for Retail::Products controller

--- test/functional/retail/products_controller_test.rb
+++ test/functional/retail/products_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class Retail::ProductsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  include Devise::TestHelpers
+
+  test "should respond with 403 for private Catalog association" do
+    get :show, :catalog_id => 3, :id => 2
+    assert_response :forbidden
+  end
+
+  test "should respond with 404 for wrong Catalog association" do
+    get :show, :catalog_id => 1, :id => 2
+    assert_response :missing
   end
 end
 
