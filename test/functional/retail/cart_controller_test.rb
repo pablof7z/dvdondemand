2526b72d52ad572f15fac0466aad4924b40614cc
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-11-02 08:37:34 -0300

    most basic functional test for adding to Cart

--- test/functional/retail/cart_controller_test.rb
+++ test/functional/retail/cart_controller_test.rb
@@ -9,10 +9,15 @@ class Retail::CartControllerTest < ActionController::TestCase
     sign_in @customer
   end
 
-  test 'empty cart when Customer just logged-in' do
+  test 'have an empty cart when Customer just logged-in' do
     get :index
     assert_response :success
     assert assigns(:cart_items).empty?
+  end
+
+  test 'add one Product to Cart and redirect' do
+    post :add_item, { 'item[catalog_id]' => '1', 'item[product_id]' => '1', 'item[qty]' => '1', 'item[price]' => '14.99' }
+    assert_redirected_to retail_cart_path
   end
 end
 
