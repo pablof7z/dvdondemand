fe5d451b740588133c3807ea2be10271810abca8
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-11-17 11:28:05 -0300

    OrderItems assoc. refactoring in Orders model & updates

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,15 +3,15 @@ class Order < ActiveRecord::Base
   belongs_to :customer
   belongs_to :shipping_option # USPS, FedEx, DHL, etc.
   has_many :customer_payments
-  has_many :order_items, :dependent => :delete_all
+  has_many :items, :class_name => 'OrderItem', :dependent => :delete_all
 
-  accepts_nested_attributes_for :order_items
+  accepts_nested_attributes_for :items
 
   validates_presence_of :billing_address
 
   def item_count
     total = 0
-    total = order_items.inject(0) do |subtotal, item|
+    total = items.inject(0) do |subtotal, item|
       subtotal + item.quantity
     end
     total
@@ -19,7 +19,7 @@ class Order < ActiveRecord::Base
 
   def total
     total = 0
-    total = order_items.inject(0) do |subtotal, item|
+    total = items.inject(0) do |subtotal, item|
       subtotal + item.product.price * item.quantity + item.packaging_option.price
     end
     total
