a635c711ea20064704f8ef14fa87c2c9f0857de7
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-11-17 14:27:17 -0300

    completing Order models for FirstData gw.

---
+++ db/migrate/20101117172335_create_order_transactions.rb
@@ -0,0 +1,18 @@
+class CreateOrderTransactions < ActiveRecord::Migration
+  def self.up
+    create_table :order_transactions do |t|
+      t.references :order
+      t.integer :amount
+      t.boolean :success
+      t.string :authorization
+      t.string :message
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :order_transactions
+  end
+end
+
