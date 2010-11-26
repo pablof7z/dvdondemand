9dfcc1f4d64f6a586e8edfda0cdd58bad0d03850
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-11-26 15:30:20 -0300

    CartItem resource creation on existing Cart resource

--- app/controllers/retail/cart_items_controller.rb
+++ app/controllers/retail/cart_items_controller.rb
@@ -2,6 +2,10 @@ class Retail::CartItemsController < RetailController
   before_filter :authenticate_customer!
   belongs_to :customer
 
+  def create
+    create! { retail_customer_cart_url(current_customer) }
+  end
+
   def destroy
     destroy! do
       current_customer.cart.destroy if current_customer.cart.items.blank?
