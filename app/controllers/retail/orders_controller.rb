f14e423e4d81b592b80af21645e3673085935a56
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-11-26 20:56:10 -0300

    fixed Order creation from full Cart resource

--- app/controllers/retail/orders_controller.rb
+++ app/controllers/retail/orders_controller.rb
@@ -3,6 +3,13 @@ class Retail::OrdersController < RetailController
   belongs_to :customer
   actions :new, :create
 
+  def new
+    # pre-build order w/items in the cart
+    @order = current_customer.orders.build
+    current_customer.cart.items.each { |item| @order.items.build(:product_id => item.product.id, :packaging_option_id => item.packaging_option.id, :price => item.product.price, :quantity => item.quantity) }
+    new!
+  end
+
   def create
     create! do |success, failure|
       success.html { current_customer.cart.destroy }
