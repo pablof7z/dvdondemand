ec425d190521c13ee07f64aff4ba19b9fd959de6
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-11-30 21:39:20 -0300

    do not allow Products in Cart to be added more than once

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -10,5 +10,10 @@ class Cart < ActiveRecord::Base
   def items_total_price
     items.sum('price * quantity')
   end
+
+  def items_include?(product)
+    items.each { |i| return true if i.product == product }
+    false
+  end
 end
 
