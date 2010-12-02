0e3ebabe515ce3613677724ea7afed2da3056946
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-02 11:51:03 -0300

    essential Cart unit test

--- test/unit/cart_test.rb
+++ test/unit/cart_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @customer = customers(:happy)
+    @product = products(:johns)
+  end
+
+  test "check cart totals" do
+    assert_equal @customer.cart.items_total_count, 3
+    assert_equal @customer.cart.items_total_price, 6.5
+  end
+
+  test 'check cart inclusion' do
+    assert @customer.cart.items_include?(@product)
   end
 end
