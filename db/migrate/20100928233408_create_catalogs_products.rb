597e2991c895414cf47c407e8e386c4fcc8f4911
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-06 15:00:29 -0300

    adding named references to fixtures

--- db/migrate/20100928233408_create_catalogs_products.rb
+++ db/migrate/20100928233408_create_catalogs_products.rb
@@ -1,8 +1,8 @@
 class CreateCatalogsProducts < ActiveRecord::Migration
   def self.up
     create_table :catalogs_products, :id => false do |t|
-      t.integer :catalog_id
-      t.integer :product_id
+      t.references :catalog
+      t.references :product
     end
   end
 
