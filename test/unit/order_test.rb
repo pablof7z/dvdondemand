dfa4c3d260ad0626092d6757408979b19c236ad2
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-07 08:21:58 -0300

    Order unit testing with named ref. fixtures

--- test/unit/order_test.rb
+++ test/unit/order_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class OrderTest < ActiveSupport::TestCase
   def setup
     @order = orders(:one)
+    @customer = customers(:happy)
   end
 
   test "composite billing address" do
@@ -11,5 +12,12 @@ class OrderTest < ActiveSupport::TestCase
 
   test "composite shipping address" do
     assert_equal @order.shipping_address, "Vucetich<br />Nieva<br />Smallville<br />UT<br />US"
+  end
+
+  test 'named referenced fixtures' do
+    assert_equal @customer.orders.size, 1
+    assert_equal @order.items_full_count, 3
+    assert_equal @order.subtotal, 37.97
+    assert_equal @order.total, 43.72
   end
 end
