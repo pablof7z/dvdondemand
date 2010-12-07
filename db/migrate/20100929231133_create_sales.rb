96ead2b480529ed9332a7d208d1db78146a338f0
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-07 11:06:23 -0300

    remodeled Sales resource w/unit testing

--- db/migrate/20100929231133_create_sales.rb
+++ db/migrate/20100929231133_create_sales.rb
@@ -2,9 +2,10 @@ class CreateSales < ActiveRecord::Migration
   def self.up
     create_table :sales do |t|
       t.references :publisher
-      t.string :sale_type
-      t.float :net_amount
-      t.float :total
+      t.references :order
+      t.string     :sale_type  # "type" name column reserved for SingleTableInheritance
+      t.integer    :quantity
+      t.float      :total
 
       t.timestamps
     end
