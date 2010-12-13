3e167f5b2f0687bd4b37620e908c23abad948777
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-12 22:27:11 -0300

    added fixtures for 3nd Order/Sale, tested

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,4 +2,7 @@ class Sale < ActiveRecord::Base
   belongs_to :publisher
   belongs_to :order
   has_many :publisher_payments
+
+  default_scope :order => 'created_at'
 end
+
--- test/unit/sale_test.rb
+++ test/unit/sale_test.rb
@@ -10,6 +10,6 @@ class SaleTest < ActiveSupport::TestCase
   test 'assoc. order sales by named ref. fixtures' do
     assert_equal @order.sales.count, 2
     assert_equal @jane.sales.count, 1
-    assert_equal 2, @john.sales.count
+    assert_equal 3, @john.sales.count
   end
 end
