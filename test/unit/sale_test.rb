ebf408019b94765d6917296e6029a3807602aa7f
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-13 21:20:40 -0300

    fixtures fixin', tested

--- test/unit/sale_test.rb
+++ test/unit/sale_test.rb
@@ -2,14 +2,25 @@ require 'test_helper'
 
 class SaleTest < ActiveSupport::TestCase
   def setup
-    @order = orders(:one)
+    @order1 = orders(:one)
+    @order2 = orders(:two)
+    @order3 = orders(:three)
+    @order4 = orders(:four)
+    @order5 = orders(:five)
     @jane = publishers(:jane)
     @john = publishers(:john)
   end
 
-  test 'assoc. order sales by named ref. fixtures' do
-    assert_equal @order.sales.count, 2
-    assert_equal @jane.sales.count, 1
-    assert_equal 3, @john.sales.count
+  test 'assoc. orders by named ref. fixtures' do
+    assert_equal 2, @order1.sales.count
+    assert_equal 1, @order2.sales.count
+    assert_equal 1, @order3.sales.count
+    assert_equal 1, @order4.sales.count
+    assert_equal 1, @order5.sales.count
+  end
+
+  test 'assoc. publisher sales by named ref. fixtures' do
+    assert_equal 1, @jane.sales.count
+    assert_equal 5, @john.sales.count
   end
 end
