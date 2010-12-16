649a4296d735b525bd214cbd71e265cf6e6f49f5
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-15 21:55:33 -0300

    grouped retail Sales by month, updated GoogleChart

--- app/controllers/publish/sales_controller.rb
+++ app/controllers/publish/sales_controller.rb
@@ -1,5 +1,11 @@
 class Publish::SalesController < PublishController
   belongs_to :publisher
   actions :index, :show
+
+  protected
+
+  def collection
+    @sales ||= end_of_association_chain.group_by { |s| s.created_at.beginning_of_month  }
+  end
 end
 
