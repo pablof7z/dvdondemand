d3fc245b6d9be8ef044e3091e24095a273a98949
Pablo Fernandez <pablo@littleq.net>
2010-12-20 16:33:54 -0300

    Add validations to catalog, show flash on layout

--- app/controllers/publish/catalogs_controller.rb
+++ app/controllers/publish/catalogs_controller.rb
@@ -1,5 +1,11 @@
 class Publish::CatalogsController < PublishController
   belongs_to :publisher
+  
+  def create
+    create! do |success, failure|
+      success.html { redirect_to publish_publisher_catalogs_path(current_publisher) }
+    end
+  end
 
   protected
 
