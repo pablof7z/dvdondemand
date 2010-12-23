e85ad3948cc2dcb8d953bfd161519c8a9ddf4fd1
Emanuel Alvarez <emanuel@serverprotectors.com>
2010-12-23 00:09:27 -0300

    added genres list to retail front page

--- app/controllers/retail/home_controller.rb
+++ app/controllers/retail/home_controller.rb
@@ -2,5 +2,7 @@ class Retail::HomeController < RetailController
   def index
     @products = Product.all(:order => :updated_at, :limit => 50).map { |p| p if p.available_for_retail_listing? }.compact
     @catalogs = Catalog.public.all(:order => :updated_at, :limit => 50).map { |c| c if c.available_for_retail_listing }.compact
+    @genres_for_cd = Genre.for_cd
+    @genres_for_dvd = Genre.for_dvd
   end
 end
