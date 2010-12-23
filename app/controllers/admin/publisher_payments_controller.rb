3242341f15cf8db5c146b9f1839d9390632ed8c0
Pablo Fernandez <pablo@littleq.net>
2010-12-23 15:56:43 -0300

    Add comments for nacha integration

--- app/controllers/admin/publisher_payments_controller.rb
+++ app/controllers/admin/publisher_payments_controller.rb
@@ -8,10 +8,18 @@ class Admin::PublisherPaymentsController < AdminController
         redirect_to generate_admin_publisher_payment_path(1) and return
       end
       
+      # Create NACHA class
+      
+      # Go through each selected publisher
       params[:publisher].each do |i|
+        # Get the publisher
         publisher = Publisher.find(i)
         sales = publisher.sales_owed
+        
+        # Create the payment
         publisher_payment = PublisherPayment.new(:publisher => publisher, :amount => publisher.owed, :memo => "Payment file created by #{current_admin.email}", :bank_information => publisher.default_bank_information)
+        
+        # Save payment
         if ! publisher_payment.save
           flash[:warning] = "Publisher payment couldn't be created for #{publisher.full_name}"
           redirect_to generate_admin_publisher_payment_path(1) and return
@@ -19,11 +27,15 @@ class Admin::PublisherPaymentsController < AdminController
           sales.each do |sale|
             sale.publisher_payment = publisher_payment
             sale.save!
-            flash[:notice] = "Payments successfully generated"
-            redirect_to admin_publisher_payments_path
+            
+            # Add payment to ach file
+            flash[:notice] = "Payments successfully generated" # XXX Get rid of this once ach is integrated
+            redirect_to admin_publisher_payments_path          # XXX Get rid of this once ach is integrated
           end
         end
       end
+      
+      # Prompt user to download ach file
     end
   end
 end
