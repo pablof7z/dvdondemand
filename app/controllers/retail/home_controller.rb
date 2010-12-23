7903b92c8b1ebac3a2849697ff9ae05b95f428f0
Emanuel Alvarez <emanuel@serverprotectors.com>
2010-12-23 00:48:17 -0300

    added best selling list

--- app/controllers/retail/home_controller.rb
+++ app/controllers/retail/home_controller.rb
@@ -1,8 +1,16 @@
 class Retail::HomeController < RetailController
   def index
-    @products = Product.all(:order => :updated_at, :limit => 50).map { |p| p if p.available_for_retail_listing? }.compact
-    @catalogs = Catalog.public.all(:order => :updated_at, :limit => 50).map { |c| c if c.available_for_retail_listing }.compact
+    @products = Product.all(:order => :updated_at, :limit => 50).map do |p|
+      p if p.available_for_retail_listing?
+    end.compact
+
+    @catalogs = Catalog.public.all(:order => :updated_at, :limit => 50).map do |c|
+      c if c.available_for_retail_listing
+    end.compact
+
     @genres_for_cd = Genre.for_cd
     @genres_for_dvd = Genre.for_dvd
+
+    @best_selling = @products.take(5)
   end
 end
