bbf283d11de2aa38e7257a496d00f63798e8314e
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-24 10:17:23 -0300

    add parenthesis to Fee amounts on reports

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # fee amounts are substractions, so add parenthesis to them
+  def fee_number_to_currency(n=0)
+    "(#{number_to_currency(n)})"
+  end
 end
