1d8286723454dbb79fca3dc4950c4866fa646a38
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-24 12:19:41 -0300

    fixed monthly grouping reports also on Ledger

--- app/controllers/publish/sales_controller.rb
+++ app/controllers/publish/sales_controller.rb
@@ -10,7 +10,7 @@ class Publish::SalesController < PublishController
   def ledger
     yy = params[:year]  || Time.now.year
     mm = params[:month] || Time.now.month
-    dd = Date.new(yy.to_i, mm.to_i, 1)
+    dd = DateTime.new(yy.to_i, mm.to_i, 1)
     start  = dd.beginning_of_month
     finish = dd.end_of_month
     @sales = current_publisher.send(params[:type] || 'retail_sales').find(:all, :conditions => {:created_at => start..finish})
