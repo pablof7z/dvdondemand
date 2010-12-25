b4201876220fcf438c6ad217ecad98224d92eb17
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-25 21:27:27 -0200

    first retail sale payment fixture-tested with refactored models

--- test/unit/publisher_test.rb
+++ test/unit/publisher_test.rb
@@ -16,8 +16,9 @@ class PublisherTest < ActiveSupport::TestCase
   test 'due payments totals for publisher' do
     assert_equal  19.98, @jane.sales.totals
     assert_equal  15.98, @jane.pending_payment_totals
+    # actually with some payments made
     assert_equal 824.45, @john.sales.totals
-    assert_equal 707.45, @john.pending_payment_totals
+    assert_equal 669.23, @john.pending_payment_totals
   end
 
   test 'due payments count for publisher' do
@@ -29,9 +30,9 @@ class PublisherTest < ActiveSupport::TestCase
     assert_equal  1, @jane.retail_sales.pending_payment.count
     # publisher with many kind of sales
     assert_equal 10, @john.sales.count
-    assert_equal 10, @john.sales.pending_payment.count
+    assert_equal  8, @john.sales.pending_payment.count
     assert_equal  2, @john.get_stocks.pending_payment.count
     assert_equal  3, @john.whole_sales.pending_payment.count
-    assert_equal  5, @john.retail_sales.pending_payment.count
+    assert_equal  3, @john.retail_sales.pending_payment.count
   end
 end
