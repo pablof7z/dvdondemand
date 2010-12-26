b682975cef70ddd3cc36aedb7fcd452285eb30e8
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-26 10:38:31 -0300

    publisher payments added to yearly/monthly reports

--- app/controllers/publish/sales_controller.rb
+++ app/controllers/publish/sales_controller.rb
@@ -23,6 +23,7 @@ class Publish::SalesController < PublishController
     @retail_sales = current_publisher.retail_sales
     @whole_sales  = current_publisher.whole_sales
     @get_stocks   = current_publisher.get_stocks
+    @payments     = current_publisher.publisher_payments
   end
 end
 
