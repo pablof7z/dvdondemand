acacbbac7e0fcd1131c6904db7e03089723c1eec
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-27 19:43:28 -0200

    versioning support to Fee model thru vestal_versions gem

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,7 @@ Rails::Initializer.run do |config|
   config.gem "inherited_resources"
   config.gem 'mime-types', :lib => "mime/types"
   config.gem "paperclip"
+  config.gem "vestal_versions"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
