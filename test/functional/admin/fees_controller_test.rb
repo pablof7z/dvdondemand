6e074cd3517fb693cbb2c6c8f66e512abab94501
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-28 13:02:20 -0200

    Admin's functional-test for soft-deleted Fees

--- test/functional/admin/fees_controller_test.rb
+++ test/functional/admin/fees_controller_test.rb
@@ -9,10 +9,11 @@ class Admin::FeesControllerTest < ActionController::TestCase
     sign_in @admin
   end
 
-  test 'available Fees' do
+  test 'available Fees (non soft-deleted)' do
     get :index
     assert_response :success
     assert_not_nil assigns(:fees)
     assert_equal 2, assigns(:fees).count
+    assert_equal 3, Fee.all.count
   end
 end
