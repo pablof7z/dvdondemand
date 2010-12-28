320329a0ca6e90e589e80a7f501d1218164558b1
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-28 20:28:11 -0300

    made current Retail functional-tests pass

--- test/functional/retail/catalogs_controller_test.rb
+++ test/functional/retail/catalogs_controller_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class Retail::CatalogsControllerTest < ActionController::TestCase
 
   test "should respond with 403 for private Catalog" do
-    get :show, :id => 3
+    @private = catalogs(:janes_private)
+    get :show, :id => @private.id
     assert_response :forbidden
   end
 
--- test/functional/retail/products_controller_test.rb
+++ test/functional/retail/products_controller_test.rb
@@ -1,18 +1,23 @@
 require 'test_helper'
 
 class Retail::ProductsControllerTest < ActionController::TestCase
+  def setup
+    @foreing = catalogs(:johns)
+    @private = catalogs(:janes_private)
+    @product = products(:janes)
+  end
 
   test "products routes always along with catalogs'" do
     assert_routing '/retail/catalogs/1/products/1', :controller => 'retail/products', :action => 'show', :catalog_id => '1', :id => '1'
   end
 
   test "should respond with 403 for private Catalog association" do
-    get :show, :catalog_id => 3, :id => 2
+    get :show, :catalog_id => @private.id, :id => @product.id
     assert_response :forbidden
   end
 
   test "should respond with 404 for wrong Catalog association" do
-    get :show, :catalog_id => 1, :id => 2
+    get :show, :catalog_id => @foreing.id, :id => @product.id
     assert_response :missing
   end
 
