97547f791ba966005d0189e03d247a9c61f0b931
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-28 14:36:36 -0200

    allow Publisher's soft-deletion of products

--- app/controllers/admin/fees_controller.rb
+++ app/controllers/admin/fees_controller.rb
@@ -2,7 +2,7 @@ class Admin::FeesController < AdminController
 
   def destroy
     Fee.find(params[:id]).update_attribute(:deleted_at, Time.now)
-    redirect_to admin_fees_path
+    redirect_to admin_fees_url
   end
 
   private
