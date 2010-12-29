ab8069573ac8ecc1fee090b5a183b9f4434821b5
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-29 10:42:37 -0300

    hide soft-deleted products from Retail catalogs

--- test/functional/retail/catalogs_controller_test.rb
+++ test/functional/retail/catalogs_controller_test.rb
@@ -1,12 +1,15 @@
 require 'test_helper'
 
 class Retail::CatalogsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
+  def setup
+    @private = catalogs(:janes_private)
+  end
 
   test "should respond with 403 for private Catalog" do
-    @private = catalogs(:janes_private)
     get :show, :id => @private.id
     assert_response :forbidden
   end
-
 end
 
