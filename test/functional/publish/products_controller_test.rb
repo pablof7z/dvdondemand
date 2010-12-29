6d9323658b28e61345089a8d7a7526f76051f739
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-29 20:11:25 -0300

    Product no longer versioned, but soft-deletion double-tested

--- test/functional/publish/products_controller_test.rb
+++ test/functional/publish/products_controller_test.rb
@@ -8,8 +8,8 @@ class Publish::ProductsControllerTest < ActionController::TestCase
     @publisher.confirm!
     sign_in @publisher
 
-    @catalog = @publisher.catalogs.first
-    @product = @publisher.products.first
+    @catalog = catalogs(:janes)
+    @product = products(:janes)
   end
 
   test 'Products index at least require a Publisher' do
@@ -38,6 +38,20 @@ class Publish::ProductsControllerTest < ActionController::TestCase
     get :index, :publisher_id => @publisher.id.to_s, :catalog_id => @catalog.id.to_s
     assert_response :success
     assert_equal @catalog.products.available, assigns(:products) 
+  end
+
+  test 'soft-deletion of product reduces products availability' do
+    assert_difference(['Product.available.count','@publisher.products.available.count'],-1) do
+      delete :destroy, :id => @product.id
+    end
+    assert_redirected_to publish_publisher_products_url(@publisher)
+  end
+
+  test 'soft-deletion of product does not actually destroy it' do
+    assert_no_difference(['Product.count','@publisher.products.count']) do
+      delete :destroy, :id => @product.id
+    end
+    assert_redirected_to publish_publisher_products_url(@publisher)
   end
 end
 
