be23dee9d33b07474d97358111764f7870fe90cd
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-30 13:54:29 -0200

    some refactoring to publisher's Sales controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ ActionController::Routing::Routes.draw do |map|
       publisher.resources :product_placements, :as => 'placements', :only => [:create]
 
       publisher.resources :genres, :only => :index
-      publisher.resources :sales, :only => [:index, :show], :collection => {:overview => :get, :ledger => :get}
+      publisher.resources :sales, :only => [:index, :show], :collection => {:ledger => :get}
 
       publisher.resources :bank_informations, :as => :financial, :member => { :send_deposit => :post, :make_default => :post, :validate => [ :get, :post, :put ] }, :except => [ :destroy ]
       publisher.resources :publisher_payments, :as => :payments, :only => [:index, :show]
