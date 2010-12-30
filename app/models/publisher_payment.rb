c68c8a4e5dd7f1355e735ad00c567a8ed4475453
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2010-12-30 18:53:10 -0300

    refactored Sales/Payments range totals

--- app/models/publisher_payment.rb
+++ app/models/publisher_payment.rb
@@ -3,6 +3,10 @@ class PublisherPayment < ActiveRecord::Base
   belongs_to :bank_information
   has_many :sales
   
+  def self.totals_for(start,finish)
+    sum(:amount, :conditions => { :created_at => start..finish }).round(2)
+  end
+
   def payment_method
     return "Bank Wire " if bank_information
     return ""
