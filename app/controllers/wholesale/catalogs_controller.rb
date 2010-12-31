1b0f0a75ef5ee0149a9db805b497dc3f0b8bc075
Pablo Fernandez <pablo@littleq.net>
2010-12-31 03:37:39 -0300

    Show catalog export form

--- app/controllers/wholesale/catalogs_controller.rb
+++ app/controllers/wholesale/catalogs_controller.rb
@@ -2,11 +2,28 @@ class Wholesale::CatalogsController < WholesaleController
   before_filter :authenticate_wholesaler!, :except => [ :index ]
   
   def index
-    @catalogs = Catalog.all
-    
-    respond_to do |format|
-      format.xml { send_data @catalogs.to_xml, :filename => "complete_catalog.xml" }
-      format.html
+    case params[:type]
+    when 'catalogs'
+      @catalogs = Catalog.all
+      
+      respond_to do |format|
+        format.xml { send_data @catalogs.to_xml(:include => :products), :filename => "catalog.xml" }
+        format.html
+      end
+    when 'packaging_options'
+      @packaging_options = PackagingOption.all
+      
+      respond_to do |format|
+        format.xml { send_data @packaging_options.to_xml, :filename => "packaging_options.xml" }
+        format.html
+      end
+    when 'shipping_options'
+      @shipping_options = ShippingOption.all
+      
+      respond_to do |format|
+        format.xml { send_data @shipping_options.to_xml, :filename => "shipping_options.xml" }
+        format.html
+      end
     end
   end
 end
