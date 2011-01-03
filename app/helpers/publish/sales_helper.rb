4ff32c64554aa12dd6a9d3dae4f0640a7a61f138
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2011-01-02 21:41:17 -0300

    refactoring some Sales reports views, functional testing

--- app/helpers/publish/sales_helper.rb
+++ app/helpers/publish/sales_helper.rb
@@ -1,4 +1,13 @@
 module Publish::SalesHelper
+  def saleize(sale)
+    # this is foobar, it should be extracted from publisher's has_many assoc. names if they were named correctly for starts
+    case sale
+      when Wholesale: 'whole_sales'
+      when GetStock: 'get_stocks'
+      when Retail: 'retail_sales'
+    end
+  end
+
   def current_or_not(action=nil)
     if block_given?
       {:class => 'current'} if yield
