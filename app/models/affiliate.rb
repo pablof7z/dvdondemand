a55d8089bbaabcb479ff68837babb6902920ebac
Pablo Fernandez <pablo@littleq.net>
2011-01-10 19:30:34 -0300

    Some cleanup

--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -18,10 +18,7 @@ class Affiliate < ActiveRecord::Base
   # all pending payments totals (minus fees)
   def pending_payment_totals
     unless sales.pending_payment_affiliate.blank?
-      sales.pending_payment_affiliate.inject(0) do |sum,s|
-        sum + (((s.total - s.fees) * commission_percentage)/100) +
-              (s.quantity * commission_per_unit)
-      end.round(2)
+      sales.pending_payment_affiliate.inject(0) { |sum, s| sum + commission(s) }.round(2)
     else
       0
     end
@@ -32,21 +29,10 @@ class Affiliate < ActiveRecord::Base
     return validated_financial_informations.first
   end
   
-  def owed_previous
-    s = 0.0
-    
-    publishers.each { |p| s = s + owed_for_publisher(p) }
-    
-    s
-  end
-  
   def paid_for_publisher(publisher)
     s = 0.0
     
-    publisher.sales.done_payment_affiliate.each do |sale|
-      s = s + (((sale.total - sale.fees) * commission_percentage)/100)
-      s = s + sale.quantity * commission_per_unit
-    end
+    publisher.sales.done_payment_affiliate.each { |sale| s = s + commission(sale) }
     
     s
   end
@@ -54,11 +40,16 @@ class Affiliate < ActiveRecord::Base
   def owed_for_publisher(publisher)
     s = 0.0
     
-    publisher.sales.pending_payment_affiliate.each do |sale|
-      s = s + (((sale.total - sale.fees) * commission_percentage)/100)
-      s = s + sale.quantity * commission_per_unit
-    end
+    publisher.sales.pending_payment_affiliate.each { |sale| s = s + commission(sale) }
     
     s
   end
+  
+  private
+  
+  def commission(sale)
+    (((sale.total - sale.fees) * commission_percentage)/100) +
+      sale.quantity * commission_per_unit
+  end
 end
+
