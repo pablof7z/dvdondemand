5387e9e9e73141bc88c5a0bf78e8660ebe23ba1d
Emanuel Alvarez <emanuel@serverprotectors.com>
2011-01-14 23:44:12 -0300

    add auto_sign_in before filter to retail controller

--- app/controllers/retail_controller.rb
+++ app/controllers/retail_controller.rb
@@ -1,4 +1,16 @@
 class RetailController < InheritedResources::Base
+  before_filter :auto_sign_in
   layout 'retail'
+
+  protected
+  def auto_sign_in
+    sign_in(:customer, Customer.anonymous) unless customer_signed_in?
+    if (cart_id = session.delete(:cart_id)) and current_customer.cart.blank?
+      begin
+        Cart.find(cart_id).assign_customer(current_customer)
+      rescue ActiveRecord::RecordNotFound
+      end
+    end
+  end
 end
 
