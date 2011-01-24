3466b7f76a42cbae369ab73104b36dbd03724d56
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2011-01-24 20:55:22 -0300

    pagination added to Orders index (Admin mod.)

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,3 +1,9 @@
 class Admin::OrdersController < AdminController
-end
+  actions :index, :show
 
+  private
+
+  def collection
+    @orders ||= end_of_association_chain.paginate :page => params[:page], :per_page => params[:per_page] || Order.per_page
+  end
+end
