ad401fbe7f6a96bb1ba23a81f459b097c7bb329f
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2011-01-28 12:56:38 -0300

    packaging options pagination added to Admin. mod, *not* tested

---
+++ test/functional/admin/packaging_options_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class Admin::PackagingOptionsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
+  def setup
+    @admin = admins(:joe)
+    # admin user is not confirmable
+    sign_in @admin
+  end
+
+  test 'a barely working index page' do
+    get :index
+    assert_response :success
+  end
+end
