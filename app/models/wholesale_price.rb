897f02f96ae29a1131349b404cd115b3186ece75
Cristian R. Arroyo <cristian.arroyo@vivaserver.com.ar>
2011-01-28 11:29:06 -0300

    wholesale prices pagination added to Admin. mod, tested

--- app/models/wholesale_price.rb
+++ app/models/wholesale_price.rb
@@ -1,5 +1,9 @@
 class WholesalePrice < ActiveRecord::Base
   validates_numericality_of :minimum_quantity, :greater_than => 1
   validates_numericality_of :discount_percentage, :greater_than => 0, :less_than => 100
-end
 
+  # set the pagination limit here, but mind the tests
+  def self.per_page
+    10
+  end
+end
