1e7a832f59eacbb5176a04a3d8c7fa7e8c52c3f3
Emanuel Alvarez <emanuel@serverprotectors.com>
2011-02-09 00:21:26 -0300

    add pagination support to orders collection

--- app/controllers/retail/orders_controller.rb
+++ app/controllers/retail/orders_controller.rb
@@ -43,6 +43,10 @@ class Retail::OrdersController < RetailController
  
   private
 
+  def collection
+    @orders ||= end_of_association_chain.paginate :page => params[:page], :per_page => params[:per_page] || Order.per_page
+  end
+
   def prevent_session_hijack
     render(:file => "#{RAILS_ROOT}/public/404.html", :status => 404) if params[:customer_id].to_i != current_customer.id
   end
