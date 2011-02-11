3f112511af0757e59f7e95bfa8297bd43ca76768
Emanuel Alvarez <emanuel@serverprotectors.com>
2011-02-10 22:21:31 -0300

    add flag action to retail product controller

--- app/controllers/retail/products_controller.rb
+++ app/controllers/retail/products_controller.rb
@@ -10,5 +10,16 @@ class Retail::ProductsController < RetailController
     # also raised when accessing a Product with incorrect Catalog association
     render(:text => 'Not Found', :status => 404)
   end
+
+  def flag
+    resource.flag_count = (resource.flag_count.nil?)? 0 : resource.flag_count += 1
+    if resource.save
+      flash[:notice] = 'The product has been flagged'
+    else
+      flash[:alert] = 'Error flagging the product'
+    end
+    redirect_to retail_catalog_product_path(resource.catalogs.first, resource)
+  end
+
 end
 
