db6a06a6725d02714d6a29266efe013ddf4ebe54
Emanuel Alvarez <emanuel@serverprotectors.com>
2011-03-01 17:47:56 -0300

    fix price bug on get stock orders

--- app/controllers/publish/get_stocks_controller.rb
+++ app/controllers/publish/get_stocks_controller.rb
@@ -5,7 +5,7 @@ class Publish::GetStocksController < PublishController
   def index
     @order = current_publisher.get_orders.build
     current_publisher.products.available.each do |p|
-      @order.items.build(:product => p, :packaging_option => nil, :quantity => 0)
+      @order.items.build(:product => p, :price => p.price, :packaging_option => nil, :quantity => 0)
     end
   end
 
