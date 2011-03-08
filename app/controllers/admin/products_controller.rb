612261bc175181db1e3f41c428203eb83142554c
Emanuel Alvarez <emanuel@serverprotectors.com>
2011-03-08 14:55:53 -0300

    add admin products show action method

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -16,7 +16,13 @@ class Admin::ProductsController < AdminController
       redirect_to admin_products_path
     end
   end
-  
+
+  def show
+    sign_out(:publisher)
+    sign_in(:publisher, resource.publisher)
+    redirect_to publish_publisher_product_path(current_publisher, @product)
+  end
+
   def collection
     @products ||= end_of_association_chain.paginate :page => params[:page], :per_page => params[:per_page] || Product.per_page
   end
