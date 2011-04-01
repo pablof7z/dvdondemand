227c1a8a4069ebf04848ba67e6817527d155db9a
Emanuel Alvarez <emanuel@serverprotectors.com>
2011-04-01 13:17:29 -0300

    add priority named scopes to order log model

--- app/models/order_log.rb
+++ app/models/order_log.rb
@@ -1,3 +1,10 @@
 class OrderLog < ActiveRecord::Base
   belongs_to :order
+
+  named_scope :debug, :conditions => "priority like 'debug'"
+  named_scope :info, :conditions => "priority like 'info'"
+  named_scope :warning, :conditions => "priority like 'warning'"
+  named_scope :error, :conditions => "priority like 'error'"
+  named_scope :fatal, :conditions => "priority like 'fatal'"
+  named_scope :unknown, :conditions => "priority like 'unknown'"
 end
