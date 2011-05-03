da2a07aa2d077d2e98571ccd40226c981bcf377b
Emanuel Alvarez <emanuel@serverprotectors.com>
2011-05-03 15:15:19 -0300

    show only retailable featured products in home page

--- app/controllers/retail/home_controller.rb
+++ app/controllers/retail/home_controller.rb
@@ -1,6 +1,6 @@
 class Retail::HomeController < RetailController
   def index
-    @products = Product.featured
+    @products = Product.featured.select { |product| product.retailable? }
     @latest_products = Product.available_filo.for_retail.last(25)
     @genres_for_cd = Genre.for_cd
     @genres_for_dvd = Genre.for_dvd
