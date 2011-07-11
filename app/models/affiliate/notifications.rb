6d9d1fbba83113cfda64eea42f097eaa7d516108
Emanuel Alvarez <emanuel@serverprotectors.com>
2011-07-11 17:21:00 -0300

    change from field and add reply-to to affiliate notifications

--- app/models/affiliate/notifications.rb
+++ app/models/affiliate/notifications.rb
@@ -1,7 +1,8 @@
 class Affiliate::Notifications < ActionMailer::Base
   def introduction(introduction)
     recipients introduction.email
-    from       "#{introduction.affiliate.name} <#{introduction.affiliate.email}>"
+    from       "#{SITE} <no-reply@#{SITE.gsub(/ /, '').downcase}.com>"
+    reply_to   "#{introduction.affiliate.name} <#{introduction.affiliate.email}>"
     subject    "Invitation to join #{SITE}"
     sent_on    Time.now
     body({ :introduction=>introduction })
